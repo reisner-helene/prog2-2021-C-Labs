92a4c6640558887d460a688341fad02dae050b69
reisner-helene <helene.reisner.001@student.uni.lu>
2021-03-12 13:58:43 +0100

    Upload Ex11 - MineSweeper - 2 dimentional array

--- Lab 3/Lab 3 Solutions/Ex 11/main.c
+++ Lab 3/Lab 3 Solutions/Ex 11/main.c
@@ -9,23 +9,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 10
-#define PROBABILITY 75
+#define N 10 //size of grid
+#define PROBABILITY 75 //proba of hitting the mine
 
-#define MINE 1
-#define EMPTY 0
-#define SET -1
+#define MINE 1 //encoding for a mine
+#define EMPTY 0 //encoding for an empty cell
+#define SET -1 
 
-int grid[N][N];
-int numberOfMines = 0;
+//global variables - be v careful with using global variables, should be inside of the functions instead! using global ones is not a good idea. 
+int grid[N][N]; //define the grid
+int numberOfMines = 0; //number of mines is zero in the beginning
 
-void fillGrid() {
-    srand(time(NULL));
+void fillGrid() { 
+    srand(time(NULL)); //seed for random generator
 
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < N; i++) //iterate thro lines and columns
         for(int j = 0; j < N; j++) {
-            grid[i][j] = (rand() % 100) < PROBABILITY;
-            numberOfMines += grid[i][j];
+            grid[i][j] = (rand() % 100) < PROBABILITY; // each element of the grid will be a random nr btw 0 and 99. it will be a boolean... if this number is btw zero and probability. if it's true it will be a mine, if false wont be a mine!
+            numberOfMines += grid[i][j]; // we cound the number of mines
         }
 }
 
@@ -34,16 +35,16 @@ void printGrid(int showMines) {
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++)
             if(showMines)
-                printf("%c ", grid[i][j] == MINE ? '*' : ' ');
+                printf("%c ", grid[i][j] == MINE ? '*' : ' '); //if you have a mine at that position, create a start, otherwise create a whitespace. if statement (condition ? yes : no) - if condition is met it will execute first part, if not no
             else
-                printf("%c ", grid[i][j] == SET ? ' ' : '?');
+                printf("%c ", grid[i][j] == SET ? ' ' : '?'); //if I dont want to show the mines (if not a mine?), this line comes in
 
         printf("\n");
     }
     printf("\n");
 }
 
-int checkGrid() {
+int checkGrid() { //checking if the whole grid has been discovered
     for(int i = 0; i < N; i++)
         for(int j = 0; j < N; j++)
             if(grid[i][j] == EMPTY)
@@ -63,26 +64,26 @@ int main() {
         printGrid(0);
 
         do {
-            printf("x = ");
+            printf("x = "); //ask user for coordinates
             scanf("%d", &x);
-        } while(x < 0 || x >= N);
+        } while(x < 0 || x >= N); //if not proper coordinates, we will ask them again
 
         do {
             printf("y = ");
             scanf("%d", &y);
         } while(y < 0 || y >= N);
 
-        if(grid[x][y] == MINE) {
+        if(grid[x][y] == MINE) { //have a look into the grid, if it's a mine then we lose
             printGrid(1);
             printf("BOOM!\n");
-            break;
+            break; //this break refers to the first upper loop (the first while or forloop in which "this" is - line 63!) So we stop the while loop of line 63!
         } else {
-            grid[x][y] = SET;
+            grid[x][y] = SET; //change the value of the grid 
 
-            if(checkGrid()) {
+            if(checkGrid()) { //checks if i won by seeing that I discovered the whole greed - otherwise it will loop again and ask for new coordinates
                 printGrid(1);
                 printf("You win!\n");
-                break;
+                break; 
             }
         }
     }
